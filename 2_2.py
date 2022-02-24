480769b0226efa8de3e93317a3037dffc42ded7c
Deeno Gonzalez <deenogonzalez@swuser-129-8-235-171.swlan.csufresno.edu>
2022-02-24 14:54:01 -0800

    adding 2.2 changes

---
+++ 2_2.py
@@ -0,0 +1,54 @@
+
+# myvar = "say it ain't so"
+# print (myvar)
+
+
+# var2 = "Span & Eggs"
+
+# spam = "    Spam
+# eggs = "Eggs      "
+# spam_and_eggs = spam + " & " + eggs
+
+# print(spam_and_eggs[:])
+
+# length = len(spam_and eggs)
+# print(length)
+# print(spam_and_eggs.upper())
+
+
+# praise = "Good Doggie"
+
+# count = praise.upper().count("G")
+# print("Count: ", count)
+
+# Dionicio Gonzalez
+#full_name = input("Enter your full name: ")
+#index_of_space = full_name.rfind(" ")
+#first_name = full_name[:index_of_space]
+#last_name = full_name[index_of_space+1:]
+#print("First name", first_name)
+#print("Last name", last_name)
+
+# annual_income = eval(input("Enter your annual income"))
+# print("User's income is ", annual_income)
+# monthly_income = annual_income/12
+# print("monthly income", monthly_income)
+
+# var1 = 1
+# var2 = 2
+# var3 = "3"
+# var4 = "4"
+
+# var5= var1+var2 #3 # Int
+# var6= var3+var4 #34 # String
+
+str1 = "Python"
+
+length = len(str1) #5 #Int
+
+str2 = str1[:] #Python String
+print("str2", str2)
+str3 = str1[:10] #Error
+print("str3", str3)
+str4 = str2[2:-1] #Tho
+print("str4", str4)
